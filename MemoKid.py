bf12385ecc7075893d27594570680b1618ea45c8
RoeiHarfi <symphonymelodysong@gmail.com>
2021-11-28 20:18:09 +0200

    Login Page done (no db yet)

--- MemoKid.py
+++ MemoKid.py
@@ -1,38 +1,51 @@
 import tkinter as tk
 import mysql.connector
-import pygame
+import pygame as pg
 from tkinter import *
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.geometry("1280x720")
 root.title("Login Page")
 
-#img=PhotoImage(file='bg.jpg')
-#Label(root,image=img).pack()
+#background image
+C = Canvas(root, bg="blue", height=1280, width=720)
+bg = PhotoImage(file = "bg.png")
+background_label = Label(root, image=bg)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+C.pack()
 
 def LoginPage():
+    #title image
+    title1=Image.open("Title1.png")
+    title = ImageTk.PhotoImage(title1)
+    label_title = tk.Label(image=title)
+    label_title.image = title
+    label_title.place(x=400, y =100)
 
     #function for login buton
     def LoginButton():
         user = ID.get()
         passw = password.get()
-        print("UserID is",user,"PW is",passw)
+        print("UserID is", user, "PW is", passw)
 
     #ID TextBox
-    lblfrstrow=tk.Label(root, text = "תעודת זהות", )
-    lblfrstrow.place(x=1100, y = 100)
+    lblfrstrow=tk.Label(root, bg='#17331b', fg='white', text = "תעודת זהות", )
+    lblfrstrow.place(x=650, y = 300)
     ID = tk.Entry(root, width = 35)
-    ID.place(x=1000, y=100, width = 100)
+    ID.place(x=550, y=300, width = 100)
 
     #PW TextBox
-    lblsecrow = tk.Label(root, text="סיסמה")
-    lblsecrow.place(x=1100, y=150)
+    lblsecrow = tk.Label(root, bg='#17331b', fg='white', text="סיסמה")
+    lblsecrow.place(x=650, y=350)
     password = tk.Entry(root, width=35)
-    password.place(x=1000, y=150, width=100)
+    password.place(x=550, y=350, width=100)
 
     #Login button
     loginbutton = tk.Button(root, text ="התחבר", command=LoginButton)
-    loginbutton.place(x=1050, y=200, width=55)
+    loginbutton.place(x=600, y=400, width=55)
+
+
 
 LoginPage()
 root.mainloop()
